b5159e2cc63c7fbfd6bc5b6223943b05de15bb3e
Park Minseong <burningmagic@naver.com>
2012-11-15 05:01:18 +0900

    구매화면 관련 리스트간에 개체 이동 구현
    xml2개 추가 생성

--- gen/com/example/embedeed_project/R.java
+++ gen/com/example/embedeed_project/R.java
@@ -34,6 +34,7 @@ public final class R {
         public static final int yellow=0x7f02000e;
     }
     public static final class id {
+<<<<<<< HEAD
         public static final int PurchaseAddButton=0x7f080012;
         public static final int PurchaseByCompanyButton=0x7f080010;
         public static final int PurchaseByDateEndDateET=0x7f080018;
@@ -78,6 +79,36 @@ public final class R {
         public static final int textView1=0x7f080002;
         public static final int totalPrice=0x7f080005;
         public static final int totalPriceText=0x7f080004;
+=======
+        public static final int SalesList=0x7f070015;
+        public static final int SalesSummary=0x7f070013;
+        public static final int addItemButton=0x7f07000b;
+        public static final int amount=0x7f070006;
+        public static final int backButton=0x7f070010;
+        public static final int barcodeButton=0x7f070011;
+        public static final int barcodeReadButton=0x7f070001;
+        public static final int button1=0x7f070014;
+        public static final int calculateButton=0x7f07000e;
+        public static final int editText1=0x7f070019;
+        public static final int editText2=0x7f07001a;
+        public static final int gridview=0x7f070000;
+        public static final int menu_settings=0x7f07001b;
+        public static final int orderItemName=0x7f070003;
+        public static final int orderList=0x7f07000d;
+        public static final int price=0x7f070008;
+        public static final int productList=0x7f07000f;
+        public static final int productName=0x7f070007;
+        public static final int resetButton=0x7f070012;
+        public static final int salesByCompanyButton=0x7f070017;
+        public static final int salesByDayButton=0x7f070016;
+        public static final int salesByProduct=0x7f070018;
+        public static final int singlePrice=0x7f070004;
+        public static final int stock=0x7f070009;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f07000a;
+        public static final int totalPrice=0x7f070005;
+        public static final int totalPriceText=0x7f07000c;
+>>>>>>> 구매화면 관련 리스트간에 개체 이동 구현
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -86,6 +117,7 @@ public final class R {
         public static final int app_info=0x7f030003;
         public static final int cafe=0x7f030004;
         public static final int option=0x7f030005;
+<<<<<<< HEAD
         public static final int products_management=0x7f030006;
         public static final int purchase_main=0x7f030007;
         public static final int purchase_management=0x7f030008;
@@ -101,6 +133,19 @@ public final class R {
     public static final class menu {
         public static final int activity_main=0x7f070000;
         public static final int pos_main=0x7f070001;
+=======
+        public static final int order_listview=0x7f030006;
+        public static final int product_gridview=0x7f030007;
+        public static final int products_management=0x7f030008;
+        public static final int purchase_main=0x7f030009;
+        public static final int purchase_management=0x7f03000a;
+        public static final int sales_management=0x7f03000b;
+        public static final int sales_management_by_day=0x7f03000c;
+        public static final int shop_info=0x7f03000d;
+    }
+    public static final class menu {
+        public static final int activity_main=0x7f060000;
+>>>>>>> 구매화면 관련 리스트간에 개체 이동 구현
     }
     public static final class string {
         public static final int app_name=0x7f040000;
