0744aaa9290fcc5b675d7dc4ca4a726438be8621
Minsik Kong <minsick10@gmail.com>
2012-11-14 20:15:25 +0900

    매출관리form작성중

--- src/com/example/embedeed_project/SalesManagement.java
+++ src/com/example/embedeed_project/SalesManagement.java
@@ -1,14 +1,41 @@
 package com.example.embedeed_project;
 
+import java.util.ArrayList;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SalesManagement extends Activity {
+public class SalesManagement extends ListActivity {
+	private ArrayList<String> list;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sales_management);
+
+		list = new ArrayList<String>();
+
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		list.add("6");
+		list.add("7");
+		list.add("8");
+		list.add("9");
+		// adapter.notifyDataSetChanged();
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+
+		setListAdapter(adapter);
 	}
 
 }
