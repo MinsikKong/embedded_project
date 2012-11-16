e89edadda3d14e429a03525f406fc0451d000453
Minsik Kong <minsick10@gmail.com>
2012-11-16 14:48:15 +0900

    1111111111111111

--- src/com/example/embedeed_project/Cafe.java
+++ src/com/example/embedeed_project/Cafe.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Cafe extends Activity {
-	//까페가기(박민성)
+	// 까페가기(박민성)
 	/**
 	 * @param args
 	 */
@@ -16,9 +16,9 @@ public class Cafe extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	
-	    // TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		// TODO Auto-generated method stub
 	}
 
 }
