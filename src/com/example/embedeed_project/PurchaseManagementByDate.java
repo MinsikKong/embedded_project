fd85291e6aae3a5fcf1e456ae2437f82b6ae5a8a
Minsik Kong <minsick10@gmail.com>
2012-11-24 21:26:11 +0900

    매입 일자별조회

--- src/com/example/embedeed_project/PurchaseManagementByDate.java
+++ src/com/example/embedeed_project/PurchaseManagementByDate.java
@@ -36,6 +36,8 @@ public class PurchaseManagementByDate extends Activity {
 	private int year, month, day;
 	static final int DATE_DIALOG_ID = 0;
 	View tempView;
+	
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
