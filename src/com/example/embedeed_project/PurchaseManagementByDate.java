38fd223dc7d5a5bd1b0e34d8017b771af9a9809f
Minsik Kong <minsick10@gmail.com>
2012-11-24 19:12:01 +0900

    일자별 조회 됨

--- src/com/example/embedeed_project/PurchaseManagementByDate.java
+++ src/com/example/embedeed_project/PurchaseManagementByDate.java
@@ -2,23 +2,35 @@ package com.example.embedeed_project;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class PurchaseManagementByDate extends Activity {
 	// 일자별 매출(공민식)
 	private ArrayList<String> list;
 	private ArrayAdapter<String> adapter;
 	private ListView productList;
+	Cursor cursor;
+	SQLiteDatabase db;
+	public int count;
+	Button search;
 
 	EditText et_startdate, et_enddate;
 	private int year, month, day;
@@ -30,38 +42,20 @@ public class PurchaseManagementByDate extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.purchase_management_by_date);
 
-		// datepicker 관련 변수들 초기화 함수
-		Init();
-
-		// 각 edittext 터치하면 DatePicker Dialog 호출함
-		et_startdate.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				tempView = v;
-				showDialog(DATE_DIALOG_ID);
-			}
-		});
-		et_enddate.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				tempView = v;
-				showDialog(DATE_DIALOG_ID);
-			}
-		});
-
 		productList = (ListView) findViewById(R.id.PurchaseByDateList);
+
+		productList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
+
 		list = new ArrayList<String>();
-
-		list.add("매출1");
-		// adapter.notifyDataSetChanged();
-
-		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, list);
-		productList.setAdapter(adapter);
-	}
-
-	// 변수들 초기화
-	public void Init() {
 		et_startdate = (EditText) findViewById(R.id.PurchaseByDateStartDateEditext);
 		et_enddate = (EditText) findViewById(R.id.PurchaseByDateEndDateEditext);
 
@@ -69,6 +63,58 @@ public class PurchaseManagementByDate extends Activity {
 		year = objTime.get(Calendar.YEAR);
 		month = objTime.get(Calendar.MONTH);
 		day = objTime.get(Calendar.DAY_OF_MONTH);
+
+		// 각 edittext 터치하면 DatePicker Dialog 호출함
+		et_startdate.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				tempView = v;
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+		et_enddate.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				tempView = v;
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+
+		search = (Button) findViewById(R.id.PurchaseByDateButton1);
+		search.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				db = openOrCreateDatabase(Const.DATABASE_NAME, MODE_PRIVATE,
+						null);
+				db.setVersion(1);
+				db.setLocale(Locale.getDefault());
+				db.setLockingEnabled(true);
+
+				// 특정 일자 사이에 있는 매입 조회
+				cursor = db.rawQuery(
+						"select * from purchase where created_at between" + "'"
+								+ et_startdate.getText() + "' and '"
+								+ et_enddate.getText() + "'", null);
+
+				// 해당 내역이 없으면 메시지 뿌림
+				if (cursor.moveToFirst()) {
+					cursor.moveToFirst();
+				} else {
+					Toast.makeText(PurchaseManagementByDate.this,
+							"해당일의 매입 내역이 없습니다", Toast.LENGTH_SHORT).show();
+				}
+
+				// select된 매입 내역을 list에 추가
+				for (count = cursor.getCount(); count > 0; count--) {
+					String item_name = cursor.getString(3);
+					list.add(item_name);
+					cursor.moveToNext();
+				}
+				adapter.notifyDataSetChanged(); // listview refresh
+				cursor.close();
+			}
+		});
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		productList.setAdapter(adapter);
 	}
 
 	protected Dialog onCreateDialog(int id) {
@@ -81,9 +127,9 @@ public class PurchaseManagementByDate extends Activity {
 		return null;
 	}
 
+	// DatePicker Dialog에서 날짜를 정하면 해당 날짜를 해당 edittext에 넣음(보여
 	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
 		public void onDateSet(DatePicker view, int year, int month, int day) {
-			// 시작일,종료일을 정했을 경우 시작일의 textview에 날자 출력
 			if (tempView.getId() == R.id.PurchaseByDateStartDateEditext) {
 				et_startdate.setText(year + "-" + (month + 1) + "-" + day);
 			} else {
